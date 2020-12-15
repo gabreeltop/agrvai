a725c821c246b0a6743f9b2d75f98f6450f509bb
Gabriel Rossi <gabrielrossi.2908@gmail.com>
2020-12-14 21:54:03 -0300

    foi

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 const Url = require('url-parse')
-const DATABASE_URL = new Url(Env.get("DATABASE_URL"))
+const DATABASE_URL = new Url(Env.get("DATABASE_URL"));
 
 module.exports = {
   /*
@@ -50,16 +50,16 @@ module.exports = {
   | npm i --save mysql
   |
   */
-  pg: {
-    client: 'pg',
+  mysql: {
+    client: "mysql",
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.hostname),
-      port: Env.get('DB_PORT', DATABASE_URL.port),
-      user: Env.get('DB_USER', DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+      host: Env.get('DB_HOST', "localhost"),
+      port: Env.get('DB_PORT', ""),
+      user: Env.get('DB_USER', "root"),
+      password: Env.get('DB_PASSWORD', ""),
+      database: Env.get('DB_DATABASE', "adonis")
     },
-    debug: Env.get('DB_DEBUG', false)
+    debug: Env.get('DB_DEBUG', false),
   },
 
   /*
@@ -72,15 +72,14 @@ module.exports = {
   | npm i --save pg
   |
   */
-  mysql: {
-    client: 'mysql',
+  pg: {
+    client: "pg",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST', DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', DATABASE_URL.port),
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
     },
-    debug: Env.get('DB_DEBUG', false)
-  }
+  },
 }
